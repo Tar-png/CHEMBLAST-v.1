d63d2eaa4c5289bb8069f566382fc604328aee3a
Tar-png <chinaphat2552r@gmail.com>
2026-03-05 22:59:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function startGame(){
     document.body.classList.add("fade-out");
 
     setTimeout(()=>{
-        window.location.href = "/maingame/game.html";
+        window.location.href = "maingame/game.html";
     },500);
 }
 
 function goContract() {
     window.open("https://www.instagram.com/t.tarzx/");
+
 }
